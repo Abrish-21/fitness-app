b5d9a7118cfa8a40479954d85a6de61931828ee6
Abrham Tilksew <abrham.tilksew@aastustudent.edu.et>
2025-03-04 23:08:08 +0300

    gracefully handle api fetching errors

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,30 +1,40 @@
-import React, { useState, useEffect } from "react";
-function Joke() {
-  const [joke, setJoke] = useState('')
+import {useState,useEffect} from "react";
+function Joke(){
+  const [joke,setJoke] = useState('');
   const fetchJoke = async ()=>{
-  const response = await fetch('https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',{
-    method: 'GET',
+    try {
+      const response = await fetch('https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',{
+  method: 'GET',
   url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
   headers: {
     'x-rapidapi-key': '90daf35750msh11588106a0e0b1bp1061d3jsn141126a69de8',
     'x-rapidapi-host': 'jokes-by-api-ninjas.p.rapidapi.com'
   }
-  }
-  )
-   const data = await response.json()
-   setJoke(data[0].joke)
+
+      })
+      if(!response.ok)
+      {
+        throw new Error('network not ok')
+      }
+  
+      const data = await response.json()
+      setJoke(data[0].joke)
+    }
+    catch {
+      console.error('Error:',error)
+    }}
+    useEffect(()=>{
+      fetchJoke()
+    },[])
+
+    return(
+    <div className="">
+      <p>Joke: </p>
+      {joke && <p>{joke}</p>}
+    </div>
+    )
+
   }
 
-  useEffect((data)=>{
-    fetchJoke();
-  },[])
-  return (
-    <div>
-      <h2>Joke of the day:</h2>
-      {<p>{joke}</p>}
-      <button type="button" >
-        see more jokes</button>
-    </div>
-  );
-}
-export default Joke;
+
+export default Joke
