2c0d7155f5be13a1c116f969635507fb5a8e0a39
Abrham Tilksew <abrham.tilksew@aastustudent.edu.et>
2025-03-04 22:31:31 +0300

    playing with api using async/await

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
 function Joke() {
-  const  handleClick = ()=>{
-    const [joke, setJoke] = useState(null);
-    useEffect(()=>  {
-      fetch('https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes', {
-        
-        method: 'GET',
-      url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
-      headers: {
-        'x-rapidapi-key': '90daf35750msh11588106a0e0b1bp1061d3jsn141126a69de8',
-        'x-rapidapi-host': 'jokes-by-api-ninjas.p.rapidapi.com'
-      }
-    
-    })
-    .then((response)=>response.json())
-    .then((data)=>{
-      setJoke(data[0].joke)
-      console.log(data)
-    })
-    
-     },[])
+  const [joke, setJoke] = useState('')
+  const fetchJoke = async ()=>{
+  const response = await fetch('https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',{
+    method: 'GET',
+  url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
+  headers: {
+    'x-rapidapi-key': '90daf35750msh11588106a0e0b1bp1061d3jsn141126a69de8',
+    'x-rapidapi-host': 'jokes-by-api-ninjas.p.rapidapi.com'
   }
- 
+  }
+  )
+   const data = await response.json()
+   setJoke(data[0].joke)
+  }
+
+  useEffect((data)=>{
+    fetchJoke();
+  },[])
   return (
     <div>
       <h2>Joke of the day:</h2>
-      {joke && <p>{joke}</p>}
-      <button type="button" onClick={handleClick}>
+      {<p>{joke}</p>}
+      <button type="button" >
         see more jokes</button>
     </div>
   );
