8515fca138ca15d28a251210283d3fc719a38e13
Abrham Tilksew <abrham.tilksew@aastustudent.edu.et>
2025-03-05 11:29:35 +0300

    create a well organized folder structure for my react project. it will be easy for future use

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,40 +1,9 @@
-import {useState,useEffect} from "react";
-function Joke(){
-  const [joke,setJoke] = useState('');
-  const fetchJoke = async ()=>{
-    try {
-      const response = await fetch('https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',{
-  method: 'GET',
-  url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
-  headers: {
-    'x-rapidapi-key': '90daf35750msh11588106a0e0b1bp1061d3jsn141126a69de8',
-    'x-rapidapi-host': 'jokes-by-api-ninjas.p.rapidapi.com'
-  }
+import React from "react"
 
-      })
-      if(!response.ok)
-      {
-        throw new Error('network not ok')
-      }
-  
-      const data = await response.json()
-      setJoke(data[0].joke)
-    }
-    catch {
-      console.error('Error:',error)
-    }}
-    useEffect(()=>{
-      fetchJoke()
-    },[])
-
-    return(
-    <div className="">
-      <p>Joke: </p>
-      {joke && <p>{joke}</p>}
-    </div>
-    )
-
-  }
-
+function Joke() {
+  return (
+    <div className=""></div>
+  )
+}
 
 export default Joke
